6fc89632551fe87039a397893bbd39e5fdc51b6e
TurkoliakVictoria <32321920+TurkoliakVictoria@users.noreply.github.com>
2018-02-26 21:06:50 +0200

    Update Task4.java

--- src/com/company/Task4.java
+++ src/com/company/Task4.java
@@ -2,15 +2,18 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Task4 {public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println(" enter a number");
-            float number=  scanner.nextFloat();
-            if(number>=0 && number<=1)
-                System.out.println("it's between 0 and 1");
-            else
-                System.out.println("No");
-
+public class Task4 {
+            
+     public static void main(String[] args) {
+     
+     Scanner scanner = new Scanner(System.in);
+     System.out.println(" enter a number");
+     float number=  scanner.nextFloat();
+            
+     if(number>=0 && number<=1)
+          System.out.println("it's between 0 and 1");
+     else
+          System.out.println("No");
 
 }
 }
