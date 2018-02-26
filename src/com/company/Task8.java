7d30a8aa5f695afb6315672f9db5a276269b88cd
TurkoliakVictoria <32321920+TurkoliakVictoria@users.noreply.github.com>
2018-02-26 21:13:57 +0200

    Update Task8.java

--- src/com/company/Task8.java
+++ src/com/company/Task8.java
@@ -3,19 +3,20 @@ package com.company;
 import java.util.Scanner;
 
 public class Task8 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){\
+        
         int a=input();
         int b=input();
 
         if(a%10==b%10)
             System.out.println("The last numbers are the same ");
-        else System.out.println("The last numbers are different");
+        else
+            System.out.println("The last numbers are different");
     }
 
     public static int input () {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter  number");
         return scanner.nextInt();
-}
+    }
 }
