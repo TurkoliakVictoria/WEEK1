8a2df6e4834d7074f080a77d696c7c4617a771ba
TurkoliakVictoria <32321920+TurkoliakVictoria@users.noreply.github.com>
2018-02-26 21:10:19 +0200

    Update Task6.java

--- src/com/company/Task6.java
+++ src/com/company/Task6.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Task6 {
 
-    public static void main(String[] args)
-    {
-        int a=input();
-        int b=input();
-        int sum=a+b;
-        if(sum>=11&&sum<=19)
+    public static void main(String[] args){
+        int a= input();
+        int b= input();
+        int sum= a+b;
+        if(sum >=11 && sum <= 19)
             System.out.println("Sum is in the limits of 11 and 19 ");
-        else System.out.println("It is not lie in the boards of 11 and 19");
+        else 
+            System.out.println("It is not lie in the boards of 11 and 19");
     }
 
     public static int input () {
