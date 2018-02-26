91d41f61fd7e80450191e3214ceef9bf86f2a34e
TurkoliakVictoria <32321920+TurkoliakVictoria@users.noreply.github.com>
2018-02-26 21:08:54 +0200

    Update Task5.java

--- src/com/company/Task5.java
+++ src/com/company/Task5.java
@@ -3,15 +3,16 @@ package com.company;
 import java.util.Scanner;
 
 public class Task5 {
-    public static void main(String[] args)
-    {
-    int a=input();
-    int b=input();
-    if(a>b)
+    public static void main(String[] args) {
+    
+    int a= input();
+    int b= input();
+    if(a > b)
         System.out.println("a is bigger,so result is "+ (a-b));
-    else if(b>a)
+    else if(b > a)
         System.out.println("b is bigger,so result is "+ (a*b));
-    else System.out.println("a=b");
+    else 
+        System.out.println("a=b");
     }
 
     public static int input () {
