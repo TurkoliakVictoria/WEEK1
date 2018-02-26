b7cd9e8d34f1bed909b7b1fd2729db5b90fad245
TurkoliakVictoria <32321920+TurkoliakVictoria@users.noreply.github.com>
2018-02-26 21:05:05 +0200

    Update Task3.java

--- src/com/company/Task3.java
+++ src/com/company/Task3.java
@@ -2,12 +2,17 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Task3 {public static void main(String[] args) {
+public class Task3 {
+    public static void main(String[] args) {
+
     System.out.println("number");
-    Scanner k = new Scanner(System.in);
-    int a = k.nextInt();
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+   
     if(a%7==0)
         System.out.println("Double"+a*2);
-    else System.out.println("There is a rest"+a%7);
+    else 
+        System.out.println("There is a rest"+a%7);
 
-}}
+    }
+}
