2ac1598b2b9343c086342ea84452a2b138a03238
TurkoliakVictoria <32321920+TurkoliakVictoria@users.noreply.github.com>
2018-02-26 21:00:13 +0200

    Update Task1.java

--- src/com/company/Task1.java
+++ src/com/company/Task1.java
@@ -3,15 +3,18 @@ import java.util.Scanner; // імпорт сканера
 public class Task1{
         public static void main(String[] args) {
 
-            Scanner k = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
             System.out.println ("Enter time from 0 to 24 ");
-            int time = k.nextInt();
-            if(time>=0&&time<=24){
-            if(time<9 || time>18)
-            System.out.println("I rest");
-            else if(time>9&&time<18)
-                System.out.println("I am at work");}
-            else System.out.println("Day has only 24 hours");
+            int time = scanner.nextInt();
+            if(time>=0 && time<=24)
+            {
+                 if(time<9 || time>18)
+                        System.out.println("I rest");
+                else if(time> 9 && time< 18)
+                        System.out.println("I am at work");
+            }
+            else 
+                System.out.println("Day has only 24 hours");
         }
     }
 
