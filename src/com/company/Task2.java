2489b74943d3b5cee9335796cc684d8faaa91f28
TurkoliakVictoria <32321920+TurkoliakVictoria@users.noreply.github.com>
2018-02-26 21:03:30 +0200

    Update Task2.java

--- src/com/company/Task2.java
+++ src/com/company/Task2.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
- System.out.print("Enter 3 numbers:\n");
- int max=0;
-        Scanner k = new Scanner(System.in);
-        int a = k.nextInt();
-        int b=k.nextInt();
-        int c=k.nextInt();
-        if(a>b&&a>c)
-            max=a;
-        else if(b>a&&b>c)
-            max=b;
-        else if(c>a&&c>b)
-            max=c;
+        System.out.print("Enter 3 numbers:\n");
+        int max=0;
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if(a > b && a > c)
+            max= a;
+        else if(b > a && b > c)
+            max = b;
+        else if(c > a && c > b)
+            max = c;
         else
             System.out.println("No max");
         System.out.println("Max "+max);
-}}
+} 
+}
