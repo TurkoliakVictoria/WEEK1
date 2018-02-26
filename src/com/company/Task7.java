59b3587ba382712ad2433f14d8485892809e4f6c
TurkoliakVictoria <32321920+TurkoliakVictoria@users.noreply.github.com>
2018-02-26 21:11:50 +0200

    Update Task7.java

--- src/com/company/Task7.java
+++ src/com/company/Task7.java
@@ -3,16 +3,14 @@ package com.company;
 import java.util.Scanner;
 
 public class Task7 {
-    public static void main(String[] args)
-    {
-        double a=input();
-        double b=input();
+    public static void main(String[] args) {
+        double a= input();
+        double b= input();
         if(a%b==0)
             System.out.println("True and result is a/b="+ (a/b));
         else
             System.out.println("False and result is a/b="+(a/b));
-
-    }
+ }
 
     public static double input () {
         Scanner scanner = new Scanner(System.in);
